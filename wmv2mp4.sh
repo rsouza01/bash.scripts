17d5c0a8373be620574fcbf7a0abc2e1636272df
Rodrigo Souza <rsouza01@gmail.com>
2016-05-22 12:03:40 -0300

    Added multiple files processing.

--- wmv2mp4.sh
+++ wmv2mp4.sh
@@ -9,7 +9,20 @@
 #
 # History:
 # Version 0.1: 2014/04/05 (rsouza) - File creation.
+# Version 0.2: 2016/05/21 (rsouza) - Added multiple file processing.
 #
 
 
-ffmpeg -i $1 -strict -2 $1.mp4
+if [ -z "$1" ] 
+then
+    for file in *.wmv; do
+	echo "Converting: ${file%.wma}..."
+	ffmpeg -i "$file" -strict -2 "${file%.wmv}.mp4"
+    done
+else
+    echo "Converting: $1..."
+    ffmpeg -i $1 -strict -2 $1.mp4
+fi
+
+exit 0
+
